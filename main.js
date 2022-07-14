855d9d5d105b12e0a15716bda019fc271b7d4320
Jacob <jacobmfune2@gmail.com>
2022-07-14 22:57:37 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+ // Declaration of variables
+let animeTitle = document.querySelector(".anime__title");
+let animeCharacter = document.querySelector(".anime__character");
+let animeQuote = document.querySelector(".anime__quote");
+let button = document.querySelector(".btn");
+
+getAnimeQuotes();
+
+async function getAnimeQuotes() {
+  // Make a fetch request to get json data from the api
+  const response = await fetch("https://animechan.vercel.app/api/rando")
+
+  // Check if response was successful or not using the .ok method
+  if (!response.ok) {
+		throw new Error(`${animeQuote.innerHTML = `HTTP error! status: ${response.status}`}`);
+	}
+
+  // Retrieve response as JSON using the json() function
+  const data = await response.json()
+
+  // Display HTMl using the API's data
+  animeTitle.innerHTML = data.anime;
+  animeCharacter.innerHTML = data.character;
+  animeQuote.innerHTML = data.quote;
+}
+
+button.addEventListener('click', getAnimeQuotes);
